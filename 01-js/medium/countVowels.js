c75ef3c48f9c68a7f79f2c2e869ed90095a18f6c
Neha Kumari <neha993185@gmail.com>
2023-12-06 07:13:07 +0530

    count vowels

--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -6,7 +6,15 @@
 */
 
 function countVowels(str) {
-    // Your code here
+  let arr1 = str.toLowerCase().split('');
+  let count = 0;
+  for(i=0; i<arr1.length; i++){
+    if(arr1[i] === 'a' || arr1[i] === 'e' || arr1[i] === 'i' || arr1[i] === 'o' || arr1[i] === 'u')
+    count++;
+    else
+      arr1[i] = 0;
+  }
+  return count;
 }
 
 module.exports = countVowels;
