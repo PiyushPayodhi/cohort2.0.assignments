226785253736fc0dfadcefe1daf8c5d3c3b1b860
Neha Kumari <neha993185@gmail.com>
2023-12-21 00:06:16 +0530

    my code

--- week-2/01-async-js/hard (promises)/4-promise-chain.js
+++ week-2/01-async-js/hard (promises)/4-promise-chain.js
@@ -6,36 +6,32 @@
  */
 
 function wait1(t) {
-       return new Promise(resolve=>{
-        setTimeout(
-            resolve
-        , t*1000);
-       })
+    let p = new Promise(function(resolve,reject){
+        setTimeout(resolve,t*1000);
+        });
+    return p;
 }
 
 function wait2(t) {
-    return new Promise(resolve=>{
-        setTimeout(
-            resolve
-        , t*1000);
-       })
+    let p = new Promise(function(resolve,reject){
+        setTimeout(resolve,t*1000);
+        });
+    return p;
 }
 
 function wait3(t) {
-    return new Promise(resolve=>{
-        setTimeout(
-            resolve
-        , t*1000);
-       })
+    let p = new Promise(function(resolve,reject){
+        setTimeout(resolve,t*1000);
+        });
+    return p;
 }
 
 function calculateTime(t1, t2, t3) {
-   let s=new Date().getTime();
-   return  wait1(t1).then(()=>{return wait2(t2);}).then(()=>{return wait3(t3)}).then(()=>{
-        let e=new Date().getTime();
-        return (e-s);
-    })
-
+    let startTime = new Date().getTime();
+    return wait1(t1).then( ()=>{return wait2(t2)}).then( ()=> {return wait3(t3)}).then(function(){
+        let endTime = new Date().getTime();
+        return (endTime- startTime);
+    });
 }
 
 module.exports = calculateTime;
